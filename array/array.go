06100b9549cff822d5c9da888046fc804a4105f8
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2025-05-22 16:10:08 -0300

    feat: criando aula de array simples

---
+++ array/array.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notas [3]int
+
+	fmt.Println(notas)
+	notas[0], notas[1], notas[2] = 7, 4, 9
+	fmt.Println(notas)
+
+	total := 0
+
+	for i := 0; i < len(notas); i++ {
+		total += notas[i]
+	}
+
+	media := total / len(notas)
+	fmt.Printf("Média: %d", media)
+
+}
