e48b02d8981dc5dddc00dfaba7fef2ed073ed795
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2025-05-26 13:17:08 -0300

    feat: aula de slice

---
+++ slice/slicesimples.go
@@ -0,0 +1,24 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"reflect"
+// )
+
+// func main() {
+
+// 	array1 := [3]int{1, 2, 3} // array
+// 	slice1 := []int{1, 2, 3}  // slice
+
+// 	fmt.Println(array1, slice1)
+// 	fmt.Println(reflect.TypeOf(array1), reflect.TypeOf(slice1))
+
+// 	array2 := [5]int{5, 18, 24, 9, 6}
+// 	slice2 := array2[1:3]
+// 	fmt.Println(array2, slice2)
+// 	slice3 := array2[:3]
+// 	fmt.Println(array2, slice3)
+
+// 	slice4 := slice2[3:]
+// 	fmt.Println(slice2, slice4)
+// }
