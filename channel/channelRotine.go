19fb56d7983c322978649a40dd5b7217008d0b23
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2025-05-28 13:29:23 -0300

    feat: exemplo channel

---
+++ channel/channelRotine.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doisTresQuatroVezes(base int, ch chan int) {
+	time.Sleep(time.Second)
+	ch <- 2 * base
+	time.Sleep(time.Second)
+	ch <- 3 * base
+	time.Sleep(3 * time.Second)
+	ch <- 4 * base
+}
+
+func main() {
+	canal := make(chan int)
+	go doisTresQuatroVezes(2, canal)
+	fmt.Println("A")
+
+	a, b := <-canal, <-canal // sincronismo
+	fmt.Println("B")
+	fmt.Println(a, b)
+
+	fmt.Println(<-canal)
+}
---
+++ channel/channelsimples.go
@@ -0,0 +1,12 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	ch := make(chan int, 1)
+// 	ch <- 1 // Escrita - [1]
+// 	<-ch    // Leitura []
+// 	// ch <- 2 // Escrita [2]
+// 	fmt.Println(<-ch)
+// }
